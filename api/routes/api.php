05a01c909915ff4e02d9746ee7ceac5c7c678d6e
joaaobr <anacricris12@gmail.com>
2023-03-20 13:00:04 -0300

    added prefix in all routes

--- api/routes/api.php
+++ api/routes/api.php
@@ -9,21 +9,24 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/image/all', [ImageController::class, 'all']);
-
-Route::post('/image/create', [ImageController::class, 'create']);
-
-Route::get('/image/{image}', function ($image) {
-    return view('show', [ 'image' => $image ]);
+Route::prefix('image')->group(function () {
+    Route::get('/all', [ImageController::class, 'all']);
+    
+    Route::post('/create', [ImageController::class, 'create']);
+    
+    Route::get('/{image}', function ($image) {
+        return view('show', [ 'image' => $image ]);
+    });
+    
+    Route::post('/delete/{id}', [ImageController::class, 'delete']);
 });
 
-Route::post('/image/delete/{id}', [ImageController::class, 'delete']);
-
-Route::get('/user/all', [UserController::class, 'all']);
-
-Route::post('/user/create', [UserController::class, 'create']);
-
-Route::post('/user/delete/{id}', [UserController::class, 'delete']);
-
-Route::post('/user/update/{id}', [UserController::class, 'update']);
-
+Route::prefix('user')->group(function () {
+    Route::get('/all', [UserController::class, 'all']);
+    
+    Route::post('/create', [UserController::class, 'create']);
+    
+    Route::post('/delete/{id}', [UserController::class, 'delete']);
+    
+    Route::post('/update/{id}', [UserController::class, 'update']);
+});
